564819ab1449abbcb4b5ff0718a85ea7c7262da7
Diamond <gforwork1803@gmail.com>
2025-06-02 07:43:38 +0700

    Add project back end bdss-system

---
+++ back-end/bdss-system/src/main/java/com/gtwo/bdss_system/BdssSystemApplication.java
@@ -0,0 +1,13 @@
+package com.gtwo.bdss_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdssSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdssSystemApplication.class, args);
+	}
+
+}
