8588817cee3a1ff9149ffd79d204e98c5fc3bf61
Diamond <gforwork1803@gmail.com>
2025-06-10 20:29:12 +0700

    Fix pull error duplicate file

--- back-end/bdss-system/src/main/java/com/gtwo/bdss_system/dto/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.gtwo.bdss_system.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    String email;
-    String password;
-}
--- back-end/bdss-system/src/main/java/com/gtwo/bdss_system/repository/AuthenticationRepository.java
+++
@@ -1,8 +0,0 @@
-package com.gtwo.bdss_system.repository;
-
-import com.gtwo.bdss_system.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthenticationRepository extends JpaRepository<Account, Long> {
-    Account findAccountByEmail(String email);
-}
