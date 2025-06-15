9f3a56cf2563540f26ea4ed33827b1a4f8b5b9a6
Diamond <gforwork1803@gmail.com>
2025-06-15 09:23:39 +0700

    #Task 2: (Quoc) Update validate BE POST, PUT Medical Facility

--- back-end/bdss-system/src/main/java/com/gtwo/bdss_system/controller/commons/MedicalFacilityAPI.java
+++ back-end/bdss-system/src/main/java/com/gtwo/bdss_system/controller/commons/MedicalFacilityAPI.java
@@ -2,9 +2,11 @@ package com.gtwo.bdss_system.controller.commons;
 
 import com.gtwo.bdss_system.dto.commons.MedicalDTO;
 import com.gtwo.bdss_system.entity.commons.MedicalFacility;
+import com.gtwo.bdss_system.enums.Status;
 import com.gtwo.bdss_system.service.commons.MedicalFacilityService;
 
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -27,12 +29,10 @@ public class MedicalFacilityAPI {
 
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
     @PostMapping
-    public ResponseEntity create(@RequestBody MedicalDTO facility) {
-        MedicalFacility mapped = modelMapper.map(facility, MedicalFacility.class);
-        System.out.println(mapped);
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        System.out.println("Authorities: " + auth.getAuthorities());
-        return ResponseEntity.ok(service.create(mapped));
+    public ResponseEntity create(@Valid @RequestBody MedicalDTO dto) {
+        MedicalFacility entity = modelMapper.map(dto, MedicalFacility.class);
+        entity.setStatus(Status.ACTIVE);
+        return ResponseEntity.ok(service.create(entity));
     }
 
     @PreAuthorize("permitAll()")
@@ -49,7 +49,7 @@ public class MedicalFacilityAPI {
 
     @PreAuthorize("hasAnyRole('ADMIN', 'STAFF')")
     @PutMapping("/{id}")
-    public MedicalFacility update(@PathVariable Long id, @RequestBody MedicalDTO medicalFacility) {
+    public MedicalFacility update(@PathVariable Long id,@Valid @RequestBody MedicalDTO medicalFacility) {
         MedicalFacility mapped = modelMapper.map(medicalFacility, MedicalFacility.class);
         return service.update(id, mapped);
     }
