d3bad9cf201ee4df7e9ed877ad6fbaeed43610dd
Diamond <gforwork1803@gmail.com>
2025-06-15 09:33:21 +0700

    #Task 2: (Quoc) Update function throw exception for put. post medical facility

--- back-end/bdss-system/src/main/java/com/gtwo/bdss_system/controller/commons/MedicalFacilityAPI.java
+++ back-end/bdss-system/src/main/java/com/gtwo/bdss_system/controller/commons/MedicalFacilityAPI.java
@@ -4,15 +4,12 @@ import com.gtwo.bdss_system.dto.commons.MedicalDTO;
 import com.gtwo.bdss_system.entity.commons.MedicalFacility;
 import com.gtwo.bdss_system.enums.Status;
 import com.gtwo.bdss_system.service.commons.MedicalFacilityService;
-
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
