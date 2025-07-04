e78d51ca20d603e961dac7ea0e959244faa14daa
Huann <huandtvse184136@fpt.edu.vn>
2025-07-04 18:23:17 +0700

    #Task5 (huan) Update FE Forgot and Reset password form, Page Emmergency for common Member and Guest

--- back-end/bdss-system/src/main/java/com/gtwo/bdss_system/dto/auth/ResetPasswordRequest.java
+++ back-end/bdss-system/src/main/java/com/gtwo/bdss_system/dto/auth/ResetPasswordRequest.java
@@ -9,7 +9,7 @@ public class ResetPasswordRequest {
     private String token;
 
     @Pattern(
-            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,}$",
             message = "Password must be at least 8 characters long and include uppercase, lowercase, number, and special character"
     )
     private String newPassword;
